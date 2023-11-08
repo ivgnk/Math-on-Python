89c168f6732de1ee425741c1078f3de2829bd9ec
Ivan Genik <IVGenik@rambler.ru>
2023-11-08 19:10:59 +0500

    Add files via upload

---
+++ reference_scipy_signal_medfilt.py
@@ -0,0 +1,28 @@
+'''
+Based on
+https://docs.scipy.org/doc/scipy/reference/signal.html#filtering
+https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.medfilt.html#scipy.signal.medfilt
+'''
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+
+def my_1D_medfilt():
+    # https://numpy.org/doc/stable/reference/random/index.html
+    rng = np.random.default_rng() # Generator start
+    x = rng.standard_normal(250)
+
+    fig, ax = plt.subplots(figsize=(10, 8))
+    plt.plot(x)
+    lst = [5, 15, 25, 35]  # range(5,30,8)
+    for i in lst:
+        print(i)
+        y = signal.medfilt(x, i)
+        plt.plot(y)
+    # y = signal.medfilt(x, 15)
+    # plt.plot(y)
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+my_1D_medfilt()
---
+++ reference_scipy_signal_order_filter.py
@@ -0,0 +1,36 @@
+'''
+Based on
+https://docs.scipy.org/doc/scipy/reference/signal.html#filtering
+https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.order_filter.html#scipy.signal.order_filter
+
+For 2-D and more dimensions
+'''
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+
+def reference_2D():
+    x = np.arange(25).reshape(5, 5)
+    domain = np.identity(3)
+    print(f'\n{domain=}')
+    print(x)
+    # array([[ 0,  1,  2,  3,  4],
+    #        [ 5,  6,  7,  8,  9],
+    #        [10, 11, 12, 13, 14],
+    #        [15, 16, 17, 18, 19],
+    #        [20, 21, 22, 23, 24]])
+    print(signal.order_filter(x, domain, 0))
+    # array([[  0.,   0.,   0.,   0.,   0.],
+    #        [  0.,   0.,   1.,   2.,   0.],
+    #        [  0.,   5.,   6.,   7.,   0.],
+    #        [  0.,  10.,  11.,  12.,   0.],
+    #        [  0.,   0.,   0.,   0.,   0.]])
+    print(signal.order_filter(x, domain, 2))
+    # array([[  6.,   7.,   8.,   9.,   4.],
+    #        [ 11.,  12.,  13.,  14.,   9.],
+    #        [ 16.,  17.,  18.,  19.,  14.],
+    #        [ 21.,  22.,  23.,  24.,  19.],
+    #        [ 20.,  21.,  22.,  23.,  24.]])
+
+
+reference_2D()
