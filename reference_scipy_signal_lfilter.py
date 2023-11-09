6613d5b9a062d0ecdcb69fdeeca3b14aa369ecd4
Ivan Genik <IVGenik@rambler.ru>
2023-11-09 19:40:26 +0500

    Add files via upload

---
+++ reference_scipy_signal_lfilter.py
@@ -0,0 +1,71 @@
+'''
+Based on
+https://docs.scipy.org/doc/scipy/reference/signal.html#filtering
+
+Filter data along one-dimension with an IIR or FIR filter
+https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.lfilter.html#scipy.signal.lfilter
+'''
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+
+def ref_1D_lfilter():
+    '''
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.lfilter.html#scipy.signal.lfilter
+    '''
+    # --(1)------------ Generate a noisy signal to be filtered:
+    import numpy as np
+    from scipy import signal
+    import matplotlib.pyplot as plt
+    rng = np.random.default_rng()
+    t = np.linspace(-1, 1, 201)
+    x = (np.sin(2 * np.pi * 0.75 * t * (1 - t) + 2.1) +
+         0.1 * np.sin(2 * np.pi * 1.25 * t + 1) +
+         0.18 * np.cos(2 * np.pi * 3.85 * t))
+    xn = x + rng.standard_normal(len(t)) * 0.08
+    # plt.plot(x,label='ini');     plt.plot(xn,label='with noise')
+    # plt.grid(); plt.legend() ;    plt.show()
+
+    # --(2)------------ Filtering
+    # Create an order 3 lowpass butterworth filter:
+    b, a = signal.butter(3, 0.05)
+    # Apply the filter to xn. Use lfilter_zi to choose the initial condition of the filter
+    zi = signal.lfilter_zi(b, a)
+    z, _ = signal.lfilter(b, a, xn, zi=zi * xn[0])
+    # Apply the filter again, to have a result filtered at an order the same as filtfilt:
+    z2, _ = signal.lfilter(b, a, z, zi=zi*z[0])
+    # Use filtfilt to apply the filter:
+    y = signal.filtfilt(b, a, xn)
+
+    # --(2)------------ Plotting
+    # Plot the original signal and the various filtered versions:
+    plt.figure
+    plt.plot(t, xn, 'b', alpha=0.75)
+    plt.plot(t, z, 'r--', t, z2, 'r', t, y, 'k')
+    plt.legend(('noisy signal', 'lfilter, once', 'lfilter, twice',
+                'filtfilt'), loc='best')
+    plt.plot(t, x, label='ini');
+    plt.grid(True)
+    plt.show()
+
+
+def my_1D_lfilter():
+    # https://numpy.org/doc/stable/reference/random/index.html
+    rng = np.random.default_rng() # Generator start
+    x = rng.standard_normal(250)
+
+    fig, ax = plt.subplots(figsize=(10, 8))
+    plt.plot(x,label='ini')
+    lst = [5, 15, 25, 35]  # range(5,30,8)
+    for i in lst:
+        print(i)
+        y = signal.wiener(x, i)
+        plt.plot(y,label='wiener '+str(i))
+    # y = signal.medfilt(x, 15)
+    # plt.plot(y)
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+ref_1D_lfilter()
+# my_1D_wienerfilt()
