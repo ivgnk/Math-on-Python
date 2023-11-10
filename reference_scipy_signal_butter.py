1b0f025efb355d81c33c61906455c0f6d450eeeb
Ivan Genik <IVGenik@rambler.ru>
2023-11-10 18:31:23 +0500

    Add files via upload

--- reference_scipy_signal_butter.py
+++ reference_scipy_signal_butter.py
@@ -32,6 +32,10 @@ https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.butter.html
 # Представление секций второго порядка БИХ-фильтра. Возвращается только в том случае output='sos'.
 
 import math
+import numpy as np
+import matplotlib.pyplot as plt
+import inspect
+from scipy import signal
 
 def ref_1_analog_filter():
     from scipy import signal
@@ -64,7 +68,31 @@ def ref_1_analog_filter():
     plt.axhline(20*math.log10(1/math.sqrt(2)), color='red')
     plt.show()
 
-def sig_gen
+def sig_gen():
+    print(inspect.currentframe().f_code.co_name)
+
+    # --(1)-- Generate a signal made up of 10 Hz and 20 Hz, sampled at 1 kHz
+    t = np.linspace(0, 1, 1000, False)  # 1 second
+    sig = np.sin(2 * np.pi * 10 * t) + np.sin(2 * np.pi * 20 * t)
+    # https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.subplots.html
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+    ax1.plot(t, sig)
+    ax1.set_title('10 Hz and 20 Hz sinusoids')
+    ax1.axis([0, 1, -2, 2])
+
+    # --(2)--
+    # Design a digital high-pass filter at 15 Hz to remove the 10 Hz tone, and apply it to the signal.
+    # (It’s recommended to use second-order sections format when filtering,
+    # to avoid numerical error with transfer function (ba) format):
+    sos = signal.butter(10, 15, 'hp', fs=1000, output='sos')
+    filtered = signal.sosfilt(sos, sig)
+    ax2.plot(t, filtered)
+    ax2.set_title('After 15 Hz high-pass filter')
+    ax2.axis([0, 1, -2, 2])
+    ax2.set_xlabel('Time [seconds]')
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == "__main__":
     # ref_1_analog_filter()
+    sig_gen()
