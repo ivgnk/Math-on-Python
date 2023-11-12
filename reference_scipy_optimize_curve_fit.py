aba43f0f3389526e50883ef561dbbd3ef0800c11
Ivan Genik <IVGenik@rambler.ru>
2023-11-12 20:49:06 +0500

    Add files via upload

--- reference_scipy_optimize_curve_fit.py
+++ reference_scipy_optimize_curve_fit.py
@@ -10,7 +10,8 @@ def func(x, a, b, c):
     return a * np.exp(-b * x) + c
 
 xdata = np.linspace(0, 4, 50)
-a = 2.5; b = 1.3; c = 0.5
+#a = 2.5; b = 1.3; c = 0.5 the_bounds = [5., 2., 1.5]
+a = 5; b = 8; c = 5; the_bounds = [10.0, 10.0, 10.0]
 y = func(xdata, a, b, c)
 rng = np.random.default_rng()
 y_noise = 0.2 * rng.normal(size=xdata.size)
@@ -18,14 +19,15 @@ ydata = y + y_noise
 plt.plot(xdata, ydata, 'b-', label='data')
 
 popt, pcov = curve_fit(func, xdata, ydata)
-print(f'{popt=}')
+print(f'not bounds {popt=}')
 # yfit= func(xdata, *popt)
 yfit= func(xdata, popt[0], popt[1], popt[2])
-plt.plot(xdata, yfit, 'r-',
-         label='fit: a=%5.3f, b=%5.3f, c=%5.3f' % tuple(popt)+' not bounds')
+plt.plot(xdata, yfit, 'r--',
+         label='fit: a=%5.3f, b=%5.3f, c=%5.3f' % tuple(popt)+' not bounds',linewidth=3)
 
 # Constrain the optimization to the region of 0 <= a <= 3, 0 <= b <= 1 and 0 <= c <= 0.5:
-popt, pcov = curve_fit(func, xdata, ydata, bounds=(0, [3., 1., 0.5]))
+popt, pcov = curve_fit(func, xdata, ydata, bounds=(0, the_bounds))
+print(f'with bounds {popt=}')
 
 plt.plot(xdata, func(xdata, *popt), 'g--',
          label='fit: a=%5.3f, b=%5.3f, c=%5.3f' % tuple(popt)+' with bounds')
--- reference_scipy_optimize_curve_fit_2.py
+++ reference_scipy_optimize_curve_fit_2.py
@@ -6,7 +6,7 @@ https://svitla.com/blog/approximation-data-by-exponential-function-on-python
 # Как применить кусочно-линейную подгонку в Python?
 # https://stackoverflow.com/questions/29382903/how-to-apply-piecewise-linear-fit-in-python
 #
-import numpy
+import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
@@ -15,17 +15,30 @@ import matplotlib.pyplot as plt
 # y = numpy.array([3, 7, 14, 16, 26, 47, 73, 84, 113, 196, 218, 310, 356, 475, 548, 645, 794,
 #                  942, 1096, 1251, 1319, 1462, 1668, 1892, 2203, 2511, 2777, 3102, 3372, 3764])
 
+def func(x, a, b,c):
+    return a * np.exp(-b * x)+c
+
+
 # in arg> 40 then error fit
-x = numpy.arange(1, 35, 1)
-y = 0.1*numpy.exp(0.08*x)
-print(y)
+a1 = 5
+b1 = 8
+c1 = 5
+st_  = 1
+end_ = 55
+x = np.arange(st_, end_, 1)
+y = a1*np.exp(b1*x)+c1
+print(x)
 
-[a, b], res1 = curve_fit(lambda x1, a, b: a * numpy.exp(b * x1), x, y)
+[a, b, c], pcov = curve_fit(func, x, y)
+print(a, b, c)
 
-y1 = a * numpy.exp(b * x)
+y1 = a * np.exp(b * x)+c
 
 plt.plot(x, y, 'b', label = 'ini')
 plt.plot(x, y1, 'r', label = 'appr', linestyle='--')
+for yy, zz in zip(y, y1):
+    print(f'{yy}  {zz}')
+
 plt.legend()
 plt.grid()
 plt.show()
