c00fd66276362d943767ee7d6416b03d2d9be5d6
Ivan Genik <IVGenik@rambler.ru>
2023-12-08 15:04:20 +0500

    Add files via upload

---
+++ SymPy/Equations_solver.py
@@ -0,0 +1,62 @@
+'''
+Linear equations solving
+https://docs.sympy.org/latest/modules/solvers/solvers.html
+'''
+
+from sympy.solvers import solve
+from sympy import Symbol
+from pprint import pp
+
+x = Symbol('x')
+y = Symbol('y')
+z = Symbol('z')
+f1 = Symbol('f1')
+f2 = Symbol('f2')
+f3 = Symbol('f3')
+
+def fun_1():
+    print(f'{solve(x**2 - 1, x) = }')   # [-1, 1]
+    print(f'{solve(x ** 2 - 4) = }')            # [-2, 2]
+    print(f'{solve([x - 3, y**2 - 1]) =}')      # [{x: 3, y: -1}, {x: 3, y: 1}]
+
+def fun_2():
+    '''
+     https://www.youtube.com/watch?v=z7-p3tfjNS8
+     x**3 - xyz = -5
+     y**3 - xyz = 2
+     z**3 - xyz = 21
+    '''
+    def f1c(x,y,z)->float:
+        return x**3 - x*y*z + 5
+
+    def f2c(x,y,z)->float:
+        return y**3 - x*y*z -2
+
+    def f3c(x,y,z)->float:
+        return y**3 - x*y*z -21
+
+    f1 = x**3 - x*y*z + 5
+    f2 = y**3 - x*y*z - 2
+    f3 = z**3 - x*y*z - 21
+    # print(f'{solve([f1, f2, f3]) =}')  # [{x: 3, y: -1}, {x: 3, y: 1}]
+    lst = solve([f1, f2, f3])
+    # pp(lst)
+    n = len(lst)
+    print(f'\n{n=}')
+
+    # testing result for 1 equation
+    for i in range(n):
+        the_sl = lst[i]
+        print('\n',i)
+        print(the_sl) # type(the_sl)
+        print('x=', the_sl[x],'  y=', the_sl[y],'  z=', the_sl[z])
+        print('x=', type(the_sl[x]), '  y=', type(the_sl[y]), '  z=', type(the_sl[z]))
+        x1 = complex(the_sl[x])
+        y1 = complex(the_sl[y])
+        z1 = complex(the_sl[z])
+        print(f'{x1=}    {y1=}    {z1=}')
+        resf = f1c(x1,y1,z1)
+        mod_res = abs(resf)
+        print(f'{ resf= }  {mod_res = }')
+
+fun_2()
