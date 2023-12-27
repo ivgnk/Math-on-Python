14fff0e40121bfe420c92b96b4b5b6da6f1931d1
Ivan Genik <IVGenik@rambler.ru>
2023-12-27 17:51:36 +0500

    Add files via upload

--- NumPy/form_operations.py
+++ NumPy/form_operations.py
@@ -1,6 +1,8 @@
 import numpy as np
 a = np.random.randint(5, size=(10, 3))
+a[3,2] = -7
 print('\n ini matr size=(10, 3)\n',a)
+
 # https://numpy.org/doc/stable/reference/generated/numpy.transpose.html
 print('after  np.transpose')
 b = np.transpose(a)
@@ -15,4 +17,12 @@ print(dd,'\n')
 print('after  np.reshape(a, (3, 10))')
 c = np.reshape(a, (3, 10))
 print(c,'\n')
+
+# min 1 var
+print(f'\n {np.min(a)=}  {np.argmin(a)=} \n')
+
+# min 2 var
+# https://stackoverflow.com/questions/30180241/numpy-get-the-column-and-row-index-of-the-minimum-value-of-a-2d-array
+s = np.unravel_index(a.argmin(), a.shape)
+print('\n min 2 var -  \n',  s)
 
